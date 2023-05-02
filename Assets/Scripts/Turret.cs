6132241216f2e46604d0ad9c0f8a4d824778f235
Kamran <kamran.vat@protonmail.com>
2023-05-02 13:01:38 +0200

    added shoot at mouse cursor as separate function

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -60,6 +60,44 @@ public class Turret : MonoBehaviour
             {
 
                 // Calculate the direction to the closest enemy
+                Vector3 direction = closestEnemy.transform.position - transform.position;
+
+                // Rotate the turret towards the closest object on the z-axis
+                // TODO: convert this all to a vector2 thing and make the following line simpler
+                Quaternion rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f);
+                transform.rotation = rotation;
+
+                // Spawn bullet above turret
+                Vector3 spawnPos = transform.position + transform.forward.normalized;
+                Instantiate(_bulletPrefab, spawnPos, transform.rotation);
+
+                yield return new WaitForSeconds(_fireDelay);
+
+            }
+            else 
+            {
+                Debug.Log(message: "No Enemy found");
+                yield return new WaitForSeconds(_scanDelay);
+            }
+
+
+        }
+
+    } 
+    
+    IEnumerator ShootAtCursor()
+    {
+        float _fireDelay = 1 / _rateOfFire;
+        float _scanDelay = 1 / _rateOfScan;
+
+        while (true)
+        {
+            // Aim at closest Enemy
+            GameObject closestEnemy = GetClosestEnemy();
+            if (closestEnemy != null)
+            {
+
+                // Calculate the direction to the closest enemy
                 //Vector3 direction = closestEnemy.transform.position - transform.position;
 
                 // Rotate the turret towards the closest object on the z-axis
